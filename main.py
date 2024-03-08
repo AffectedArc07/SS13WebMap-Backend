d2ebbaca82b64ea9c1841f386ee4977fa3f6815d
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2024-03-08 20:19:05 +0000

    Effigy changes

--- main.py
+++ main.py
@@ -271,10 +271,10 @@ codebases = [
         Map("Tramstation", "tramstation", "_maps/map_files/tramstation/tramstation.dmm", [1, 2]),
         Map("MiniStation", "ministation", "_maps/map_files/MiniStation/MiniStation.dmm", [1]),
         Map("NorthStar", "northstar", "_maps/map_files/NorthStar/north_star.dmm", [1, 2, 3, 4]), 
-        Map("Birdshot", "birdshot", "_maps/map_files/Birdshot/birdshot.dmm", [1]), 
         Map("IceBox", "icebox", "_maps/map_files/IceBoxStation/IceBoxStation.dmm", [1, 2, 3]),
         Map("MetaStation", "metastation", "_maps/map_files/MetaStation/MetaStation.dmm", [1]), 
         Map("RimPoint", "rimpoint", "_maps/map_files/RimPoint/RimPoint.dmm", [1, 2]), 
+        Map("SigmaOctantis", "sigmaoctantis", "_maps/map_files/SigmaOctantis/SigmaOctantis.dmm", [1, 2]), 
     ]),
 
     Codebase("SS220", "ss220", "https://api.github.com/repos/ss220-space/Paradise/git/trees", "master220", "SS220-Paradise", True, "_maps", [
